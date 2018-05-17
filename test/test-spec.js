125c093177bfbb429592fa2a2484294c4f1afe89
Stefaan Ponnet <stefaan@ponnet.com>
2018-05-17 15:01:18 +0200

    added spec + basic validation unit test

---
+++ test/test-spec.js
@@ -0,0 +1,20 @@
+const expect = require("chai").expect;
+const mainspec = require("../spec/metadata-spec.json");
+
+const Validator = require('jsonschema').Validator;
+
+
+
+describe("JSON schema tests()", function() {
+	it("a manifest file should be validated", function() {
+
+		const instance = require('../examples/metadata-manifest.json');
+
+		let v = new Validator();
+		let validation = v.validate(instance, mainspec);
+
+		expect(validation.errors.length).to.equal(0);
+
+	});
+
+});
