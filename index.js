57a4f8af5d9df44bb5bbeae00136d858c5bd876e
Stefaan Ponnet <stefaan@ponnet.com>
2018-06-06 22:32:28 +0200

    updated data format for ENS published consortium parameters

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const Validator = require('jsonschema').Validator;
 
 module.exports = {
 	validate: (instance) => {
-		let v = new Validator();
-		return v.validate(instance, mainspec);
+		return new Promise((resolve, reject) => {
+			let v = new Validator();
+			let result = v.validate(instance, mainspec);
+			if (result && result.errors.length === 0) {
+				return resolve();
+			}
+			return reject(new Error(result.errors));
+		});
 	},
 };
-
--- test/test-spec.js
+++ test/test-spec.js
@@ -3,27 +3,26 @@ const mainspec = require('../spec/metadata-spec.json');
 const Validator = require('jsonschema').Validator;
 
 describe('JSON schema tests()', function() {
-
 	let v = new Validator();
 
 	it('a parameters file should be validated', () => {
 		const instance = require('../examples/metadata-parameters.json');
 		let validation = v.validate(instance, mainspec);
-		expect(validation.errors.length).to.equal(0);
+		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
 	it('a manifest file should be validated', function() {
 		const instance = require('../examples/metadata-manifest.json');
 		let validation = v.validate(instance, mainspec);
-		expect(validation.errors.length).to.equal(0);
+		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
 	it('a addmember file should be validated', function() {
 		const instance = require('../examples/metadata-addmember.json');
 		let validation = v.validate(instance, mainspec);
-		expect(validation.errors.length).to.equal(0);
+		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
 	it('a removemember file should be validated', function() {
 		const instance = require('../examples/metadata-removemember.json');
 		let validation = v.validate(instance, mainspec);
-		expect(validation.errors.length).to.equal(0);
+		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
 });
