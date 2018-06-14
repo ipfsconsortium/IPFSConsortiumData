424708deec12677e2af5f614236713c06c991e7a
Stefaan Ponnet <stefaan@ponnet.com>
2018-06-14 16:07:15 +0200

    fix: created new data formats & schema’s

--- test/test-spec.js
+++ test/test-spec.js
@@ -5,23 +5,13 @@ const Validator = require('jsonschema').Validator;
 describe('JSON schema tests()', function() {
 	let v = new Validator();
 
-	it('a parameters file should be validated', () => {
-		const instance = require('../examples/metadata-parameters.json');
+	it('a consortium file should be validated', () => {
+		const instance = require('../examples/metadata-consortium.json');
 		let validation = v.validate(instance, mainspec);
 		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
 	it('a manifest file should be validated', function() {
 		const instance = require('../examples/metadata-manifest.json');
-		let validation = v.validate(instance, mainspec);
-		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
-	});
-	it('a addmember file should be validated', function() {
-		const instance = require('../examples/metadata-addmember.json');
-		let validation = v.validate(instance, mainspec);
-		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
-	});
-	it('a removemember file should be validated', function() {
-		const instance = require('../examples/metadata-removemember.json');
 		let validation = v.validate(instance, mainspec);
 		expect(validation.errors, validation.errors).to.be.an('array').that.is.empty;
 	});
